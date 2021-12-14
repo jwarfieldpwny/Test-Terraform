79a0bdf2822177463df379b81324b5ae2058dfdb
Thomas Johander <tjohander@splunk.com>
2021-12-14 11:46:47 -0600

    Creates a single dashboard group, one dashboard, 4 charts and a detector.  Added README.md

--- TestModule/child-dashboard.tf
+++ TestModule/child-dashboard.tf
@@ -1,21 +1,3 @@
-resource "signalfx_text_chart" "title0" {
-  name     = " "
-  markdown = <<-EOF
-    <table width="100%" rules="none">
-    <tbody><tr>
-    <td align="left" bgcolor="#7200ca" height="80px">
-    <font size="5" color="#ffffff">An Example Dashboard</font>
-    </td>
-    </tr>
-    <tr>
-    <td align="left" bgcolor="#aa00ff" height="40px">
-    <font size="3" color="#ffffff">This is an example dashboard created via Terraform configuration files.</font>
-    </td>
-    </tr>
-    </tbody></table>
-  EOF
-}
-
 # note the value for dashboard_group that references the `id` property of the signalfx_dashboard_group created in ../main.tf
 resource "signalfx_dashboard" "dashboard0" {
   name            = "${var.sfx_prefix} Dashboard 0"
@@ -37,22 +19,56 @@ resource "signalfx_dashboard" "dashboard0" {
   }
 
   chart {
-    chart_id = signalfx_single_value_chart.activekpods0.id
-    width    = 3
+    chart_id = signalfx_text_chart.title0.id
+    width    = 12
     height   = 1
-    row      = 2
-    column   = 3
+    row      = 0
+  }
+
+  chart {
+    chart_id = signalfx_single_value_chart.activekpods0.id
+    width    = 6
+    height   = 1
+    row      = 1
+    column   = 0
+  }
+
+  chart {
+    chart_id = signalfx_single_value_chart.hostcount0.id
+    width    = 6
+    height   = 1
+    row      = 1
+    column   = 6
   }
 
   chart {
     chart_id = signalfx_time_chart.latencychart0.id
-    width    = 3
+    width    = 12
     height   = 1
     row      = 2
-    column   = 3
+    column   = 0
   }
+
+
 }
 
+resource "signalfx_text_chart" "title0" {
+  name     = " "
+  markdown = <<-EOF
+    <table width="100%" rules="none">
+    <tbody><tr>
+    <td align="left" bgcolor="#7200ca" height="80px">
+    <font size="5" color="#ffffff">An Example Dashboard</font>
+    </td>
+    </tr>
+    <tr>
+    <td align="left" bgcolor="#aa00ff" height="40px">
+    <font size="3" color="#ffffff">This is an example dashboard created via Terraform configuration files.</font>
+    </td>
+    </tr>
+    </tbody></table>
+  EOF
+}
 resource "signalfx_single_value_chart" "activekpods0" {
   name         = "Active K8s Pods"
   description  = "This may include \"pause\" containers used internally by K8s"
@@ -63,6 +79,20 @@ resource "signalfx_single_value_chart" "activekpods0" {
     label        = "A"
     color        = "blue"
     value_suffix = " Pods"
+  }
+  max_precision = 3
+}
+
+resource "signalfx_single_value_chart" "hostcount0" {
+  name         = "Host Count"
+  description  = "Number of Hosts SignalFx has seen in last 10 minutes"
+  program_text = <<-EOF
+    A = data('sf.org.numResourcesMonitored', filter=filter('resourceType', 'host')).publish(label='A')
+  EOF
+  viz_options {
+    label        = "A"
+    color        = "blue"
+    value_suffix = " Hosts"
   }
   max_precision = 3
 }
